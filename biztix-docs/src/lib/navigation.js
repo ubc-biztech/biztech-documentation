d9febd872ae86fb0e04a3dca6ee642c7d68b6c14
bennypc <52959626+bennypc@users.noreply.github.com>
2025-08-07 14:00:50 -0700

    create a page guide

--- biztix-docs/src/lib/navigation.js
+++ biztix-docs/src/lib/navigation.js
@@ -14,44 +14,50 @@ export const navigation = [
         title: 'Markdown Guide',
         href: '/docs/documentation-guide/markdown-guide',
       },
-      { title: 'Example Page 1', href: '/docs/example-page-1' },
-      { title: 'Example Page 2', href: '/docs/example-page-2' },
-    ],
-  },
-  {
-    title: 'Users',
-    links: [
-      { title: 'Overview', href: '/docs/users' },
-      { title: 'Create User', href: '/docs/users/create' },
-      { title: 'Check User', href: '/docs/users/check-user' },
       {
-        title: 'Check Membership',
-        href: '/docs/users/check-user-membership',
+        title: 'Example Page 1',
+        href: '/docs/documentation-guide/example-page-1',
       },
       {
-        title: 'Get User',
-        href: '/docs/users/get-user',
-      },
-      {
-        title: 'Update User',
-        href: '/docs/users/update-user',
-      },
-      {
-        title: 'Get All Users',
-        href: '/docs/users/get-all',
-      },
-      {
-        title: 'Favourite Event',
-        href: '/docs/users/favourite-event',
-      },
-      {
-        title: 'Delete User',
-        href: '/docs/users/delete-user',
+        title: 'Example Page 2',
+        href: '/docs/documentation-guide/example-page-2',
       },
     ],
   },
-  {
-    title: 'QR Codes',
-    links: [{ title: 'QR Code', href: '/docs/qr' }],
-  },
+  // {
+  //   title: 'Users',
+  //   links: [
+  //     { title: 'Overview', href: '/docs/users' },
+  //     { title: 'Create User', href: '/docs/users/create' },
+  //     { title: 'Check User', href: '/docs/users/check-user' },
+  //     {
+  //       title: 'Check Membership',
+  //       href: '/docs/users/check-user-membership',
+  //     },
+  //     {
+  //       title: 'Get User',
+  //       href: '/docs/users/get-user',
+  //     },
+  //     {
+  //       title: 'Update User',
+  //       href: '/docs/users/update-user',
+  //     },
+  //     {
+  //       title: 'Get All Users',
+  //       href: '/docs/users/get-all',
+  //     },
+  //     {
+  //       title: 'Favourite Event',
+  //       href: '/docs/users/favourite-event',
+  //     },
+  //     {
+  //       title: 'Delete User',
+  //       href: '/docs/users/delete-user',
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'QR Codes',
+  //   links: [{ title: 'QR Code', href: '/docs/qr' }],
+  // },
 ]
