1abdafec855a571f5eea7702c3029bc024a028ef
brianadhitya <brianvalentinoadhitya@gmail.com>
2026-01-18 17:58:04 -0800

    Add to path

--- biztix-docs/src/lib/navigation.js
+++ biztix-docs/src/lib/navigation.js
@@ -34,6 +34,10 @@ export const navigation = [
         title: 'Investments API',
         href: '/docs/investments/',
       },
+      {
+        title: 'Quizzes API',
+        href: '/docs/quizzes/',
+      },
     ],
   },
   // {
